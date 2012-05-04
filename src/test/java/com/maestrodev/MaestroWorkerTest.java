3f35b9a0ee06df0489433858adc41add20f49086
Carlos Sanchez <csanchez@maestrodev.com>
2012-05-04 09:24:42 +0200

    Update junit to use annotations

--- src/test/java/com/maestrodev/MaestroWorkerTest.java
+++ src/test/java/com/maestrodev/MaestroWorkerTest.java
@@ -1,35 +1,37 @@
 package com.maestrodev;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.fusesource.stomp.client.BlockingConnection;
-import org.fusesource.stomp.client.Stomp;
+import static org.fusesource.stomp.client.Constants.*;
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.TransportConnector;
+import org.fusesource.stomp.client.BlockingConnection;
+import org.fusesource.stomp.client.Stomp;
 import org.fusesource.stomp.codec.StompFrame;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import static org.fusesource.stomp.client.Constants.*;
 import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for MaestroWorker.
  */
 public class MaestroWorkerTest 
-    extends TestCase
 {
     
     BrokerService broker = new BrokerService();
     
-    @Override
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         // configure the broker
         Thread thread = new Thread () {
             public void run(){
@@ -48,39 +50,17 @@ public class MaestroWorkerTest
         thread.start();
         
         Thread.sleep(10000);
-        
-        super.setUp();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        broker.stop();
+    @After
+    public void tearDown() throws Exception {
+        if (broker != null) {
+            broker.stop();
+        }
         Thread.sleep(1000);
-        super.tearDown();
-    }
-    
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MaestroWorkerTest( String testName )
-    {
-       super( testName );
-        
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MaestroWorkerTest.class );
-    }
-
-    /**
-     * 
-     */
+    @Test
     public void testWriteOutput() throws IOException, URISyntaxException, ParseException
     {
 
@@ -120,7 +100,7 @@ public class MaestroWorkerTest
        
     }
     
-    
+    @Test
     public void testSetField() throws IOException, URISyntaxException, ParseException
     {
 
@@ -138,9 +118,7 @@ public class MaestroWorkerTest
         assertEquals(worker.getField("some field"), "some value");
     }
     
-        /**
-     * 
-     */
+    @Test
     public void testCancel() throws IOException, URISyntaxException, ParseException
     {
 
@@ -179,10 +157,8 @@ public class MaestroWorkerTest
         assertTrue(workitem.get("__cancel__").equals("true"));
        
     }
-    
-        /**
-     * 
-     */
+
+    @Test
     public void testSetWaiting() throws IOException, URISyntaxException, ParseException
     {
 
@@ -221,11 +197,7 @@ public class MaestroWorkerTest
         assertTrue(workitem.get("__waiting__").equals("true"));
     }
 
-    
-    
-     /**
-     * 
-     */
+    @Test
     public void testUpdateFieldsInRecord() throws IOException, URISyntaxException, ParseException
     {
 
@@ -269,9 +241,7 @@ public class MaestroWorkerTest
         assertTrue(workitem.get("__record_value__").equals("value"));
     }
     
-         /**
-     * 
-     */
+    @Test
     public void testCreateRecordWithFields() throws IOException, URISyntaxException, ParseException
     {
 
@@ -316,9 +286,7 @@ public class MaestroWorkerTest
         assertTrue(workitem.get("__record_values__").equals("value"));
     }
     
-          /**
-     * 
-     */
+    @Test
     public void testDeleteRecord() throws IOException, URISyntaxException, ParseException
     {
 
